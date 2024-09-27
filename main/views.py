90afd1c9a842b0a23ff3cd3baf3e2cceefa598bb
xd43vild69 <xd43vild69@gmail.com>
2024-09-27 16:37:51 -0500

    Add ui render with matrix from scale

--- main/views.py
+++ main/views.py
@@ -2,20 +2,48 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .manager import Manager
 from .instrument import Instrument
-
+from .note import Note
 
 # Create your views here.
+
+
 def home(request):
-    matrix = [[(i * 12) + j + 1 for j in range(12)] for i in range(6)]
-    #return render(request, 'matrix.html', {'matrix': matrix})
+
+    scale = ['W', 'W', 'H', 'W', 'W', 'H', 'W', 'W']
+    managerTool = Manager(Instrument.Guitar)
+    managerTool.set_strings_tool()
+    managerTool.set_scale('E', scale)
+
+    matrix = set_matrix(managerTool.tool_strings)
+    # m = managerTool.tool_strings
+
+    # return render(request, 'matrix.html', {'matrix': matrix})
     return render(request, 'home.html', {'matrix': matrix})
+
+
+def set_matrix(tool_string):
+    matrix = [[Note("") for j in range(13)] for i in range(6)]
+    interval = 1
+    for i1, ts in enumerate(reversed(tool_string)):
+        for i2, n in enumerate(ts):
+
+            if n.step != 0:
+                print(f"{n.name} is {n.step}")
+                matrix[i1][i2] = n
+                interval +=1 
+
+            if interval > 8:
+                interval = 1
+                break          
+
+    return matrix
 
 
 def button_action(request):
     if request.method == "POST":
         # Perform your server-side action here
         # Example: Print a message, update the database, etc.
-        #print("Button clicked!")
+        # print("Button clicked!")
         call_manager()
 
         # You can return a response or redirect to another page
@@ -23,20 +51,19 @@ def button_action(request):
     else:
         # Redirect or return an error if this URL is accessed without a POST request
         return redirect('/')
-    
+
+
 def call_manager():
     managerTool = Manager(Instrument.Guitar)
     managerTool.set_strings_tool()
 
-    #scale = ['H', 'W', 'W', 'H', 'W', 'W', 'W', 'H']
+    # scale = ['H', 'W', 'W', 'H', 'W', 'W', 'W', 'H']
     scale = ['W', 'W', 'H', 'W', 'W', 'H', 'W', 'W']
-    
-    tool =  managerTool.set_scale('A', scale)
-    
+
+    managerTool.set_scale('E', scale)
+
     for s in managerTool.tool_strings:
         print("string")
-        for n in s:            
+        for n in s:
             if n.step != 0:
                 print(f"{n.name} is {n.step}")
-    
-
