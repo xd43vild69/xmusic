7bc0a2a4c8c4ca497513e6908dfa3c631e9d8632
xd43vild69 <xd43vild69@gmail.com>
2024-09-27 19:28:24 -0500

    Clean code and dark mode

--- main/views.py
+++ main/views.py
@@ -7,39 +7,56 @@ from .note import Note
 # Create your views here.
 
 
+def get_scale(selected_scale):
+    scale = []
+    if selected_scale == "s1":
+        scale = ['H', 'W', 'W', 'H', 'W', 'W', 'W', 'H']
+    elif selected_scale == "s2":
+        scale = ['W', 'W', 'H', 'W', 'W', 'H', 'W', 'W']
+    return scale
+
 def home(request):
 
-    scale = ['W', 'W', 'H', 'W', 'W', 'H', 'W', 'W']
+    scale = get_scale("s1")
     managerTool = Manager(Instrument.Guitar)
     managerTool.set_strings_tool()
     managerTool.set_scale('E', scale)
-
     matrix = set_matrix(managerTool.tool_strings)
-    # m = managerTool.tool_strings
-
-    # return render(request, 'matrix.html', {'matrix': matrix})
     return render(request, 'home.html', {'matrix': matrix})
 
+def button_action(request):
+    selected_scale = None  # Default to None if nothing is selected yet
+
+    if request.method == 'POST':
+        selected_scale = request.POST.get('scale')  # 'color' is the name of the select element
+        selected_key = request.POST.get('key')
+
+    scale = get_scale(selected_scale)
+    managerTool = Manager(Instrument.Guitar)
+    managerTool.set_strings_tool()
+    managerTool.set_scale(selected_key, scale)
+    matrix = set_matrix(managerTool.tool_strings)
+
+    return render(request, 'home.html', {'matrix': matrix})
 
 def set_matrix(tool_string):
     matrix = [[Note("") for j in range(13)] for i in range(6)]
-    interval = 1
-    for i1, ts in enumerate(reversed(tool_string)):
-        for i2, n in enumerate(ts):
+    for row_index, ts in enumerate(reversed(tool_string)):
+        interval = 1
+        for col_index, n in enumerate(ts):
 
             if n.step != 0:
                 print(f"{n.name} is {n.step}")
-                matrix[i1][i2] = n
+                matrix[row_index][col_index] = n
                 interval +=1 
 
             if interval > 8:
-                interval = 1
                 break          
 
     return matrix
 
 
-def button_action(request):
+def button_action2(request):
     if request.method == "POST":
         # Perform your server-side action here
         # Example: Print a message, update the database, etc.
